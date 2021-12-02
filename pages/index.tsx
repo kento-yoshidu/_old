c028aaa1c189a7c03716b76611669f6edb27254a
kento <kento@toriwatari.bird>
2021-12-02 20:47:48 +0900

    🏃‍♂️ getStaticPathsを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,10 @@
-const RootPage = () => {
-  return <h1>It works!</h1>;
-};
+import Link from "next/link"
+
+const RootPage = () => (
+  <>
+    <h1>It works!</h1>
+    <Link href="/about">about</Link>
+  </>
+)
 
 export default RootPage;
