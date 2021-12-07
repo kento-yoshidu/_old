394b7ca571afc0af8593e19ce82a6937b35e8105
kento <kento@toriwatari.bird>
2021-12-07 19:54:45 +0900

    🏃‍♂️ naviコンポーネントを作成

---
+++ components/nav.tsx
@@ -0,0 +1,7 @@
+const Nav = () => (
+  <nav>
+    <h1>Site name</h1>
+  </nav>
+)
+
+export default Nav
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,14 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import Nav from "../components/nav"
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="ja">
         <Head />
+
+        <Nav />
+
         <body>
           <Main />
           <NextScript />
