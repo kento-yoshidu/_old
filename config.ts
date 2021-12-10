d976b0596f02196c2a6944e81a34fa3b99fbefaa
kento <kento@toriwatari.bird>
2021-12-10 22:03:13 +0900

    ✨ Update

--- config.ts
+++ config.ts
@@ -1,6 +1,8 @@
+import path from "path"
+
 const SiteMetadata = {
   siteName: "怪文書置き場",
-  postDir: "posts",
+  postDir: path.join(process.cwd(), "contents/posts"),
 }
 
 export default SiteMetadata
--- lib/posts.ts
+++ lib/posts.ts
@@ -1,15 +1,33 @@
 import fs from "fs"
 import path from "path"
 
+import config from "../config";
+import { getMatterResult } from "./helper";
+
+const postDir = config.postDir
+
+/*
+* 
+*/
 export const getAllPostSlugs = () => {
-  const dir = path.join(process.cwd(), "posts")
-  const dirNames = fs.readdirSync(dir)
-  console.log(dirNames)
+  const dirNames = fs.readdirSync(path.join(process.cwd(), "posts"))
   return dirNames.map(dirName => {
     return {
       params: {
-        slug: dirName
+        slug: dirName.replace(/\.md$/, "")
       }
     }
   })
+}
+
+export async function getPostData(slug: string) {
+  const matterResult = getMatterResult(path.join(postDir, slug, "index.md"));
+  return {
+    slug: slug,
+    content: matterResult.content,
+    category: matterResult.data.category,
+    emoji: matterResult.data.emoji,
+    thumbnail: matterResult.data.thumbnail || null,
+    ...matterResult.data,
+  };
 }
