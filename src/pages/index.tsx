42efd5eee4e6b6f61b7a307736acba8f95a5e67d
kento <kento@toriwatari.bird>
2022-03-05 11:33:36 +0900

    ✨ #39 getPostBySlug関数を作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import React from "react"
 
 import { getPostSlugs } from "../lib/getPostSlugs"
+import { getPostBySlug } from "../lib/getPostBySlug"
 
 interface Props {
   test: any
 }
+
+type Item = {
+  slug: string;
+  content: string;
+  title: string;
+  date: string;
+  tags: string[];
+};
 
 const IndexPage: React.VFC<Props> = ({ test }) => (
   <div>
@@ -23,7 +32,9 @@ export async function getStaticProps() {
   })(require.context("../data", true, /\.md$/))
   */
 
-  console.log("===", getPostSlugs())
+  const result: Item = getPostBySlug("01", ["slug", "content", "title"])
+
+  console.log(result)
 
   return {
     props: {
