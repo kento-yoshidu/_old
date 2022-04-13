086cae5965780245efae42bd4a88823b1f7fb903
kento <kento@toriwatari.bird>
2022-04-13 20:00:13 +0900

    ✋ #62 WIP

--- src/lib/getTags.ts
+++ src/lib/getTags.ts
@@ -4,10 +4,15 @@ import { getAllPosts } from "./getAllPosts"
  * 全てのタグを収集し返す
  */
 
+interface Tag {
+  tag: string
+  count: number
+}
+
 export const getTags = () => {
   const allTags = getAllPosts(["tags"])
 
-  let tags: string[] = []
+  let tags: Tag[] = []
 
   allTags.forEach((post: Item) => {
     post.tags.map((tag) => {
