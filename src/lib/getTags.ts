a67ef39eb9ca6b8a50c3b42d3a573b3902ad294e
kento <kento@toriwatari.bird>
2022-04-13 20:15:24 +0900

    🔧 #62 無駄なmapを削除

--- src/lib/getTags.ts
+++ src/lib/getTags.ts
@@ -4,20 +4,13 @@ import { getAllPosts } from "./getAllPosts"
  * 全てのタグを収集し返す
  */
 
-interface Tag {
-  tag: string
-  count: number
-}
-
 export const getTags = () => {
   const allTags = getAllPosts(["tags"])
 
-  let tags: Tag[] = []
+  let tags: string[] = []
 
   allTags.forEach((post: Item) => {
-    post.tags.map((tag) => {
-      tags = [...tags, ...post.tags]
-    })
+    tags = [...tags, ...post.tags]
   })
 
   const setTags = [...new Set(Array.from(tags))]
