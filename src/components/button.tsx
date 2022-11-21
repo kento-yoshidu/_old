4e00192d81dbc1337e2748c6ea6ce28339ecac1b
kento <kento@toriwatari.bird>
2022-11-21 20:08:57 +0900

    ✨ CSS修正

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export const Button = () => (
   <div className="text-center">
     <Link href="/">
-      <a className="text-4xl border-2 text-gray-500 border-gray-500 py-4 px-12 hover:animate-light">
+      <a className="text-xl border-2 text-gray-500 border-gray-500 py-3 px-8 hover:animate-light">
         HOME
       </a>
     </Link>
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,7 @@
 import React from "react"
 
-import * as Styles from "../styles/footer.module.scss"
-
 export const Footer = () => (
-  <footer className="flex justify-center content-center py-10 bg-black">
-    <h2 className="text-white text-2xl md:text-3xl tracking-wider">Developed by Next.js & TypeScript</h2>
+  <footer className="flex justify-center content-center py-8 bg-black">
+    <h2 className="text-white text-xl md:text-xl tracking-wider">Developed by Next.js & TypeScript</h2>
   </footer>
 )
