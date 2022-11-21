993cbfc6a99183fc8d37059627aae86f8a2361d1
kento <kento@toriwatari.bird>
2022-11-21 21:25:00 +0900

    � #128 Install twin.macro

---
+++ src/styles/markdownStyle.ts
@@ -0,0 +1,11 @@
+/* @ts-ignore */
+import { css } from "@emotion/css"
+import tw from "twin.macro"
+
+export const RichText = css`
+  h1 {
+    border-bottom: 5px solid var(--main-color);
+    font-size: clamp(1.5rem, 3vw, 3rem);
+    line-height: 1.4;
+  }
+`
